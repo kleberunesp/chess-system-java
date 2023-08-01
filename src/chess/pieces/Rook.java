4d137ab7518014feacd5d8d6d89fc2ef7749f004
kleberunesp <kleber.c.silva@unesp.br>
2023-08-01 14:45:47 -0300

    Pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -18,7 +18,6 @@ public class Rook extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
 		Position p = new Position (0,0);
 		
 		//above
